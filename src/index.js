a6e18177953cbd97c9a6941f236aded61b1196e6
Алина <>
2023-04-26 21:42:49 +0300

    docs: add project's files

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import keyboardData from './keyboard'; 
---
+++ src/keyboard.js
@@ -0,0 +1,252 @@
+const keyboardData = [
+	{
+		code: 'Digit1',
+	},
+
+	{
+		code: 'Digit2',
+	},
+
+	{
+		code: 'Digit3',
+	},
+
+	{
+		code: 'Digit4',
+	},
+
+	{
+		code: 'Digit5',
+	},
+
+	{
+		code: 'Digit6',
+	},
+
+	{
+		code: 'Digit7',
+	},
+
+	{
+		code: 'Digit8',
+	},
+
+	{
+		code: 'Digit9',
+	},
+
+	{
+		code: 'Digit0',
+	},
+
+	{
+		code: 'Minus',
+	},
+
+	{
+		code: 'Equal',
+	},
+
+	{
+		code: 'Backspace',
+	},
+
+	{
+		code: 'Tab',
+	},
+
+	{
+		code: 'keyQ',
+	},
+
+	{
+		code: 'keyW',
+	},
+
+	{
+		code: 'keyE',
+	},
+
+	{
+		code: 'keyR',
+	},
+
+	{
+		code: 'keyT',
+	},
+
+	{
+		code: 'keyY',
+	},
+
+	{
+		code: 'keyU',
+	},
+
+	{
+		code: 'keyI',
+	},
+
+	{
+		code: 'keyO',
+	},
+
+	{
+		code: 'keyP',
+	},
+
+	{
+		code: 'BracketLeft',
+	},
+
+	{
+		code: 'BracketRight',
+	},
+
+	// {
+	// 	code: 'Delete',
+	// },
+
+	{
+		code: 'CapsLock',
+	},
+
+	{
+		code: 'KeyA',
+	},
+
+	{
+		code: 'KeyS',
+	},
+
+	{
+		code: 'KeyD',
+	},
+
+	{
+		code: 'KeyF',
+	},
+
+	{
+		code: 'KeyG',
+	},
+
+	{
+		code: 'KeyH',
+	},
+
+	{
+		code: 'KeyJ',
+	},
+
+	{
+		code: 'KeyK',
+	},
+
+	{
+		code: 'KeyL',
+	},
+
+	{
+		code: 'Semicolon',
+	},
+
+	{
+		code: 'Quote',
+	},
+
+	{
+		code: 'Backslash',
+	},
+
+	{
+		code: 'Enter',
+	},
+
+	{
+		code: 'ShiftLeft',
+	},
+
+	{
+		code: 'KeyZ',
+	},
+
+	{
+		code: 'KeyX',
+	},
+
+	{
+		code: 'KeyC',
+	},
+
+	{
+		code: 'KeyV',
+	},
+
+	{
+		code: 'KeyB',
+	},
+
+	{
+		code: 'KeyN',
+	},
+
+	{
+		code: 'KeyM',
+	},
+
+	{
+		code: 'Comma',
+	},
+
+	{
+		code: 'Period',
+	},
+
+	{
+		code: 'Slash',
+	},
+
+	{
+		code: 'ArrowUp',
+	},
+
+	{
+		code: 'ShiftRight',
+	},
+
+	{
+		code: 'AltLeft',
+	},
+
+	{
+		code: 'MetaLeft',
+	},
+
+	{
+		code: 'Space',
+	},
+
+	{
+		code: 'MetaRight',
+	},
+
+	{
+		code: 'AltRight',
+	},
+
+	{
+		code: 'ArrowLeft',
+	},
+
+	{
+		code: 'ArrowDown',
+	},
+
+	{
+		code: 'ArrowRight',
+	},
+	
+];
+
+export default keyboardData;
